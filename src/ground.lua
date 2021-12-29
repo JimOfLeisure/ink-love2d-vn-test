f65510a9b3fc1caac2f15054401dfacb3e943da1
Jim Nelson <jim@jimnelson.us>
2021-12-29 12:29:13 -0800

    trying, failing to give ground angular momentum not a permanent feature, but I don't get why it's not working when setAngle does

--- src/ground.lua
+++ src/ground.lua
@@ -10,12 +10,21 @@ function Ground:load(world)
     self.body = love.physics.newBody(world, 400, 500)
     self.shape = love.physics.newRectangleShape(700, 50)
     self.fixture = love.physics.newFixture(self.body, self.shape)
-    self.body:setAngle(0.2)
+    -- self.body:setAngle(0.2)
 end
 
-function Ground:draw()
+function Ground:normal_draw()
     graphics.setColor(0.28, 0.63, 0.05)
     graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
 end
+
+function Ground:init_draw()
+    self.body:setAngularVelocity(0.2)
+    print("hi")
+    self.draw = self.normal_draw
+    self:normal_draw()
+end
+
+Ground.draw = Ground.init_draw
 
 return Ground
