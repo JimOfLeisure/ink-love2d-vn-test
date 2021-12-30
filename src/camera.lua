98a50eeb129df5df86aa36e9d12b50196baa6520
Jim Nelson <jim@jimnelson.us>
2021-12-29 23:58:19 -0800

    better nonrotated ball tracking

--- src/camera.lua
+++ src/camera.lua
@@ -3,22 +3,21 @@ camera = {
     y = 0
 }
 
--- TODO: adjust this based on configurable screen size
-local x_offset = 375
-local y_offset = 375
 local graphics = love.graphics
 local push = graphics.push
 local pop = graphics.pop
 local translate = graphics.translate
 local rotate = graphics.rotate
 local scale = graphics.scale
+local x_offset = graphics.getWidth() / 2
+local y_offset = graphics.getHeight() / 2
 
 function camera:set(angle)
     push()
     translate(-self.x, -self.y)
     -- rotate(angle)
     -- translate((-x_offset) * math.cos(angle) + x_offset, x_offset * math.sin(angle))
-    -- translate(x_offset,-y_offset)
+    translate(x_offset,y_offset)
     -- translate(-self.x + x_offset, -self.y)
     -- translate(-self.x, -self.y)
 end
