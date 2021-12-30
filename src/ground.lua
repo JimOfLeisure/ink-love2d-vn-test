18311ab0f52158563ecd6632e49cb379245dde0e
Jim Nelson <jim@jimnelson.us>
2021-12-29 20:59:27 -0800

    checkpoint have one section plus original ground

--- src/ground.lua
+++ src/ground.lua
@@ -10,8 +10,29 @@ local fixture
 local origin = (love.math.random() -0.5) * 3000
 local NOISE_SCALE = 1
 local BUMP_SCALE = 10
+local ground_sections = {}
+
+local function new_ground_section(world, x, y)
+    local section = {}
+    section.body = love.physics.newBody(world, x, t, "static")
+
+    local coords = {}
+    for i=x-50,x+50,20 do
+        table.insert(coords, i)
+        table.insert(coords, y-25 - (love.math.noise(origin + i) * NOISE_SCALE) * BUMP_SCALE)
+    end
+    for _, e in ipairs({ x+50, y+25, x-50, y+25 }) do
+        table.insert(coords, e)
+    end
+    
+    section.shape = love.physics.newPolygonShape(coords)
+    section.fixture = love.physics.newFixture(section.body, section.shape)
+
+    return section
+end
 
 function Ground:load(world)
+    table.insert(ground_sections, new_ground_section(world, 400, 500))
     self.body = love.physics.newBody(world, 400, 500, "static")
     -- self.shape = love.physics.newRectangleShape(700, 50)
     -- local coords = { -350, 50, 350, 60, 350, 100 }
@@ -36,6 +57,9 @@ end
 function Ground:draw()
     graphics.setColor(0.28, 0.63, 0.05)
     graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+    for _, section in ipairs(ground_sections) do
+        graphics.polygon("fill", section.body:getWorldPoints(section.shape:getPoints()))
+    end
 end
 
 return Ground
