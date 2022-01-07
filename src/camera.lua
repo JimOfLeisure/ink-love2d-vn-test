fb95143d53aa1741609a6df1287da8f4732c1eb7
Jim Nelson <jim@jimnelson.us>
2022-01-06 23:10:19 -0800

    camera push/pop fix

--- src/camera.lua
+++ src/camera.lua
@@ -13,12 +13,14 @@ local x_offset = graphics.getWidth() / 2 - 100
 local y_offset = graphics.getHeight() / 2 - 100
 
 function camera:set(angle)
+    push()
     rotate(-(angle - (math.pi / 2)))
     translate(-self.x, -self.y)
     translate(x_offset,y_offset)
 end
 
 function camera:unset()
+    pop()
 end
 
 return camera
